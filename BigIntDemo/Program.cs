5bb5544837b78e4aa9627b5b0d4c1663ea69e238
Fred <domaine75@gmail.com>
2017-10-10 17:08:24 +0200

    Add sample big integer

--- BigIntDemo/Program.cs
+++ BigIntDemo/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Numerics;
 
 namespace BigIntDemo
 {
@@ -9,10 +9,18 @@ namespace BigIntDemo
     {
       Action<string> display = Console.WriteLine;
       display("Big Int demo");
+      BigInteger bigNumber = new BigInteger();
+      bigNumber = BigInteger.Parse(long.MaxValue.ToString());
+      display($"int64 max value has the value of {bigNumber.ToString()}");
 
 
       display("Press any key to exit:");
       Console.ReadKey();
+    }
+
+    private bool IsPrime(BigInteger big1)
+    {
+      
     }
   }
 }
