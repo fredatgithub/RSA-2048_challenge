b7539e8bc75bae7b538054171a184be0347d756b
Fred <domaine75@gmail.com>
2017-10-10 16:10:21 +0200

    refactor unit test method

--- UnitTestGetPrimeFiles/UnitTestGetPrimeFile.cs
+++ UnitTestGetPrimeFiles/UnitTestGetPrimeFile.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.CodeDom;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestGetPrimeFiles
 {
@@ -14,13 +15,14 @@ namespace UnitTestGetPrimeFiles
       Assert.AreEqual(result, expected);
     }
 
-    [TestMethod()]
+    [TestMethod]
     public void TestMethod_Add_To_Name()
     {
       PrivateType privateTypeObject = new PrivateType(typeof(GetPrimeFiles.FormMain));
+      const string methodName = "AddToName";
       const string source = "primes1.txt";
       const string expected = "primes1-Lined.txt";
-      object obj = privateTypeObject.InvokeStatic("AddToName", source);
+      object obj = privateTypeObject.InvokeStatic(methodName, source);
       Assert.AreEqual(expected, (string)obj);
     }
   }
