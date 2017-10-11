b3b8048b719795b1d43ab7c5ab98e44a45118030
Fred <domaine75@gmail.com>
2017-10-11 09:37:36 +0200

    Add prime method

--- BigIntDemo/Program.cs
+++ BigIntDemo/Program.cs
@@ -8,19 +8,51 @@ namespace BigIntDemo
     private static void Main()
     {
       Action<string> display = Console.WriteLine;
+      Action<string> displayOneLine = Console.Write;
       display("Big Int demo");
       BigInteger bigNumber = new BigInteger();
       bigNumber = BigInteger.Parse(long.MaxValue.ToString());
       display($"int64 max value has the value of {bigNumber.ToString()}");
+      for (BigInteger i = 0; i < 100; i++)
+      {
+        if (IsPrime(i))
+        {
+          displayOneLine($"{i.ToString()} ");
+        }
+      }
 
-
+      display(string.Empty);
       display("Press any key to exit:");
       Console.ReadKey();
     }
 
-    private bool IsPrime(BigInteger big1)
+    private static bool IsPrime(BigInteger bigInt)
     {
-      
+      bool result = true;
+      if (bigInt < 2)
+      {
+        return false;
+      }
+
+      if (bigInt == 2 || bigInt == 3 || bigInt == 5)
+      {
+        return true;
+      }
+
+      if (bigInt % 2 == 0)
+      {
+        return false;
+      }
+
+      for (int i = 3; i < Math.Sqrt((double)bigInt); i = i + 2)
+      {
+        if (bigInt % i == 0)
+        {
+          return false;
+        }
+      }
+
+      return result;
     }
   }
 }
